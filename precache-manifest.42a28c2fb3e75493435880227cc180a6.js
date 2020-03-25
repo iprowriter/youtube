42e379eca6c376bc8290dae7d5d36d6d8ba3f2c2
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-03-25 16:55:19 +0100

    Updates

---
+++ precache-manifest.42a28c2fb3e75493435880227cc180a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613a13879a0de7278d347138ddc92026",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "6d6c7184858f327a79fd",
+    "url": "/youtube/static/css/main.0196c210.chunk.css"
+  },
+  {
+    "revision": "a05730ec8a378781cf5e",
+    "url": "/youtube/static/js/2.76a7e749.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube/static/js/2.76a7e749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6c7184858f327a79fd",
+    "url": "/youtube/static/js/main.051669b2.chunk.js"
+  },
+  {
+    "revision": "49bb040344d3da7a3afb",
+    "url": "/youtube/static/js/runtime-main.e5cde201.js"
+  }
+]);
